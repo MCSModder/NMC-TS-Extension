fbf60f758806275ccea24e9bf0c9714acc06ae83
TierneyJohn <Tierney-John@hotmail.com>
2023-03-30 05:46:43 +0800

    [modify] refs#5 添加 ESLint 支持及相关配置

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,5 @@
 ### Features
 
 - [[1](https://github.com/MCSModder/NMC-TS-Extension/issues/1)] 项目初始化及初期文件导入
+- [[3](https://github.com/MCSModder/NMC-TS-Extension/issues/3)] 优化 runner-nmc 导出结构
+- [[5](https://github.com/MCSModder/NMC-TS-Extension/issues/5)] 添加 ESLint 支持及相关配置
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,9 +5,11 @@ import { BaseEnv } from "./env-base";
 import { NMCEnv } from "./env-nmc";
 import { BaseRunner } from "./runner-base";
 import { NMCRunner } from "./runner-nmc";
+
 declare global {
   interface Runner extends BaseRunner, NMCRunner {}
   interface Env extends BaseEnv, NMCEnv {}
 }
+
 export interface Runner extends BaseRunner, NMCRunner {}
-export interface Env extends BaseEnv {}
+export interface Env extends BaseEnv, NMCEnv {}
